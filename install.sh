5f94dd6756681d866f998a15d1382484830bfa81
qwe7002 <qwe7002@hotmail.com>
2017-11-26 22:21:11 +0800

    修改安装脚本

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ $(basename `pwd`) != "templates" ];then
+    echo "[Error] Please do this in the templates directory!"
+    exit
+fi
+if [ ! -d "clearision" ]; then
+    git clone https://github.com/SilverBlogTeam/clearision.git --depth 1
+    cd clearision
+fi
+ln -s $(pwd)/static ../static/clearision
+if [ -f "config.example.json" ]; then
+    cp config.example.json config.json
+    vim config.json
+fi
