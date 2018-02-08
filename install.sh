f574b09b074c15e16cd1b610a49b364046af526a
jianghua huang <qwe7002@hotmail.com>
2018-02-09 01:49:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+templates_name="clearision"
 if [ $(basename `pwd`) != "templates" ];then
     echo "[Error] Please do this in the templates directory!"
     exit
 fi
-if [ ! -d "clearision" ]; then
-    git clone https://github.com/SilverBlogTheme/clearision.git --depth 1
+if [ ! -d ${templates_name} ]; then
+    git clone https://github.com/SilverBlogTeam/${templates_name}.git
 fi
-ln -sv ../clearision/static ./static/clearision
-cd clearision
-if [ -f "config.example.json" ]; then
+ln -sv ../${templates_name}/static ./static/${templates_name}
+cd ${templates_name}
+if [ -f "config.json" ]; then
     cp config.example.json config.json
     vim config.json
 fi
