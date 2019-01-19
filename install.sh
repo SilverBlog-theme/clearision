211559af95461d0ce25cb525410a84a704b08319
qwe7002 <qwe7002@hotmail.com>
2019-01-19 19:28:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ fi
 if [ ! -d ${templates_name} ]; then
     git clone https://github.com/SilverBlogTheme/${templates_name}.git
 fi
+if [[ ! -L ./static/${templates_name} ]]; then
 ln -sv ../${templates_name}/static ./static/${templates_name}
+fi
 cd ${templates_name}
 if [ -f "config.example.json" ]; then
     cp config.example.json config.json
